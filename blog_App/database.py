08800c4d8049faa141cdd3ef30734ae65b055e8f
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2025-06-24 15:21:58 +0530

    created a databaseConnection

---
+++ blog_App/database.py
@@ -0,0 +1,15 @@
+# step-1 Create Engine
+# step-2 Declare a Mapping i.e. declaritive_base
+# step-3 create a session using sessionMaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMU_DATABASE_URL = 'sqlite:/// ./blog.db'
+
+engine = create_engine(SQLALCHEMU_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(bind= engine,  autoflush= False, autocommit= False, )
+
+Base = declarative_base()
